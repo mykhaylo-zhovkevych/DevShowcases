7981b938029485d8e1073ad5a4abd1eb7c7ea2d9
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2026-02-21 15:50:39 +0100

    added temp code

--- physical-oriented-portfolio/code/temp/cocktail-website/src/App.jsx
+++ physical-oriented-portfolio/code/temp/cocktail-website/src/App.jsx
@@ -1,22 +1,48 @@
 import React from 'react'
-import GsapTo from './examples/GsapTo.jsx'
-import GsapFrom from './examples/GsapFrom.jsx'
-import GsapFromTo from './examples/GsapFromTo.jsx'
-import GsapTimeline from './examples/GsapTimeline.jsx'
-import GsapStagger from './examples/GsapStagger.jsx'
-import GsapScrollTrigger from './examples/GsapScrollTrigger.jsx'
-import GsapText from './examples/GsapText.jsx'
+import gsap from 'gsap';
+// import GsapTo from './examples/GsapTo.jsx'
+// import GsapFrom from './examples/GsapFrom.jsx'
+// import GsapFromTo from './examples/GsapFromTo.jsx'
+// import GsapTimeline from './examples/GsapTimeline.jsx'
+// import GsapStagger from './examples/GsapStagger.jsx'
+// import GsapScrollTrigger from './examples/GsapScrollTrigger.jsx'
+// import GsapText from './examples/GsapText.jsx'
+
+import {ScrollTrigger, SplitText } from "gsap/all"
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art'
+import Menu  from './components/Menu';
+import Contact from './components/Contact';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 
 export default function App() {
   return (
     <>
-      <GsapText />
+      {/* <GsapText />
       <GsapTo />
       <GsapFrom />
       <GsapFromTo />
       <GsapTimeline />
       <GsapStagger />
-      <GsapScrollTrigger />
+      <GsapScrollTrigger /> */}
+
+        <main>
+          <Navbar />
+          <Hero />
+          <Cocktails />
+          <About />
+          <Art />          
+          <Menu />
+          <Contact />
+
+
+        </main>
+
     </>
   )
 }
---
+++ physical-oriented-portfolio/code/temp/cocktail-website/src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../constants";
+import {ScrollTrigger, SplitText } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP);
+
+
+const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        });
+
+        navTween.fromTo('nav', {backgroundColor: 'transparent'},
+            {
+                backgroundColor: '#00000050',
+                backdropFilter: "blur(10px)",
+                duration: 1,
+                ease: 'power1.inOut'
+        });
+    })
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <img src="/images/logo.png" alt="logo" />
+                    <p>Velvet Pour</p>
+                </a>
+
+
+            <ul>
+                {/* the parenthesize return li automatically */}
+                {navLinks.map((link) => (
+                    <li key={link.id}>
+                        <a href={`#${link.id}`}>{link.title}</a>
+
+                    </li>
+                ))}            
+            </ul>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar
